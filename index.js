b695620d31277df6fc9a6683128d0135f8919f33
RidgelineFire <dwayne@narrativeinc.ca>
2025-02-14 22:59:14 +0000

    Added app.use(express.static(__dirname + '/public')); to express.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 const port = 3000;
 const API_URL = "https://api.lyrics.ovh/v1/";
 
-app.use(express.static("public"));
+app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
